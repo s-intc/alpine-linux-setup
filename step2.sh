1175fffe1abb3191116398c180763140b55cd310
s-intc <98427507+s-intc@users.noreply.github.com>
2022-01-27 17:03:12 -0500

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,17 +1,9 @@
 #!/bin/ash
 
-
 # add packages
 apk add doas nano xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter dbus adwaita-icon-theme gvfs udisks2 elogind polkit-elogind
 apk add feh accountsservice openvpn
 apk add firefox-esr
-apk add docker docker-compose
-
-# add user
-adduser intc wheel
-
-# add intc to wheel
-cat ./etc/doas.conf >> permit persist :wheel
 
 # create home directories
 mkdir -p /home/intc/Documents
@@ -19,19 +11,14 @@ mkdir -p /home/intc/Downloads
 mkdir -p /home/intc/.wallpapers
 mkdir -p /home/intc/.scripts
 mkdir -p /home/intc/.lauchers
-mkdir -p /home/intc/.app
 mkdir -p /home/intc/.sandbox
 mkdir -p /home/intc/.ssh
-
-# add intc to wheel
-cat ./etc/doas.conf >> permit persist :wheel
 
 # setup xorg display
 setup-xorg-base
 
 # user setup intc
 cp ./alpine-wallpaper.jpg /home/intc/.wallpapers/alpine-wallpaper.jpg
-cp ./script-login.sh /home/intc/.scripts/login-script.sh
 
 # change file ownership from root to intc
 chown -R intc:intc /home/intc
@@ -43,16 +30,3 @@ echo "background=/home/intc/.wallpapers/alpine-wallpaper.jpg" >> /etc/lightdm/li
 cp ./intc/intc /var/lib/AccountsService/users
 chown root:root /var/lib/AccountsService/users/intc
 
-# add user to docker
-addgroup intc docker
-
-# enable copy paste in vmware
-chmod g+s /usr/bin/vmware-user-suid-wrapper
-
-# give intc write access to /opt dir
-chown intc:intc /opt
-
-# mkdir /opt/docker
-mkdir -p /opt/docker
-cp ./docker/* /opt/docker/
-chown intc:intc /opt/docker
