7ed2fde80b7ecf48733844fb5a4d17fcfb1a362c
s-intc <98427507+s-intc@users.noreply.github.com>
2022-01-27 17:03:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,31 @@
 #!/bin/ash
 
-echo "========== step1  ================"
-./step1.sh
-echo "========== step2  ================"
-./step2.sh
-echo "========== step3  ================"
-./step3.sh
+# add packages
+apk add doas nano xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter dbus adwaita-icon-theme gvfs udisks2 elogind polkit-elogind
+apk add feh accountsservice openvpn
+apk add firefox-esr
+
+# create home directories
+mkdir -p /home/intc/Documents
+mkdir -p /home/intc/Downloads
+mkdir -p /home/intc/.wallpapers
+mkdir -p /home/intc/.scripts
+mkdir -p /home/intc/.lauchers
+mkdir -p /home/intc/.sandbox
+mkdir -p /home/intc/.ssh
+
+# setup xorg display
+setup-xorg-base
+
+# user setup intc
+cp ./alpine-wallpaper.jpg /home/intc/.wallpapers/alpine-wallpaper.jpg
+
+# change file ownership from root to intc
+chown -R intc:intc /home/intc
+
+# greeter background
+echo "background=/home/intc/.wallpapers/alpine-wallpaper.jpg" >> /etc/lightdm/lightdm-gtk-greeter.conf
+
+# set background image in accountsservice
+cp ./intc/intc /var/lib/AccountsService/users
+chown root:root /var/lib/AccountsService/users/intc
